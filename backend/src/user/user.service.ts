a66a921ebebc256ad5c858e2e7efaba498ad3f7f
RanylFoumbi <ranylfoumbi@gmail.com>
2024-06-19 16:32:33 +0200

    test ci

--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Injectable } from '@nestjs/common';
 import { UserModel } from './entity/user.entity';
 import { CreateUserDto } from './dto/create-user.dto';
