779ef522e960ba4a8c301f07d62a09ca31c18ed5
RanylFoumbi <ranylfoumbi@gmail.com>
2024-07-11 06:13:15 +0200

    dokerize the app just remain the frontend

---
+++ wait-for-postgres.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+set -e
+
+host="$1"
+user="$2"
+shift
+shift
+cmd="$@"
+
+until PGPASSWORD=$POSTGRES_PASSWORD psql -h "$host" -U "$user" -c '\q'; do
+  >&2 echo "Postgres is unavailable"
+  sleep 1
+done
+
+>&2 echo "Postgres is up - executing command"
+
+exec $cmd
